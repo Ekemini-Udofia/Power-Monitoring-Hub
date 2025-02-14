4f23a3f1fef64021d10865f36c8dfdb6c52f9a30
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-14 23:02:39 +0100

    Included the main libraries and stuff for code

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
 #include <Arduino.h>
-#include <RH_ASK.h>
 #include <SPI.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 
-RH_ASK driver;
-AsyncWebServer server(80);
-
-String receivedData = "";
 const char* ssid = "ekeminiudofia";
 const char* password = "ekeminietuk";
 
-void setup() {
+void setup() 
+{
     Serial.begin(115200);
-    if (!driver.init()) {
-        Serial.println("Failed to initialize driver");
-    }
-
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED && millis() < 10000) {
         delay(1000);
         Serial.println("Connecting to WiFi...");
     }
     Serial.println("Connected to WiFi");
-
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/html", "<html><body><h1>Data: " + receivedData + "</h1></body></html>");
-    });
-    server.begin();
 }
 
-void loop() {
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-    uint8_t buflen = sizeof(buf);
+void loop() 
+{
 
-    if (driver.recv(buf, &buflen)) {
-        receivedData = String((char*)buf);
-        Serial.println("Received: " + receivedData);
-    }
 }
